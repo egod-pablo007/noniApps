dfe93635e922c8a4a60791e6b7c5e45399df122c
Emmanuel Awuah <emmanuelawuah@emmanuels-macbook.local>
2018-10-28 14:28:58 +0000

    Initial Commit

---
+++ noniApps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  noniApps
+//
+//  Created by Emmanuel Awuah on 28/10/2018.
+//  Copyright © 2018 Hub360 Multimedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
